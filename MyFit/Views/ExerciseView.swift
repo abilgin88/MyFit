517f28a4f71f1325590d0c6693253e327d84ebbf
Abdullah Bilgin <96279901+abilgin88@users.noreply.github.com>
2023-04-26 06:34:35 +0300

    Create exercise and welcome swift file and Grouping files

---
+++ MyFit/Views/ExerciseView.swift
@@ -0,0 +1,20 @@
+//
+//  ExerciseView.swift
+//  MyFit
+//
+//  Created by Abdullah Bilgin on 4/26/23.
+//
+
+import SwiftUI
+
+struct ExerciseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExerciseView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExerciseView()
+    }
+}
