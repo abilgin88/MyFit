b794d745a68d5f521f28c4b8041e1cdbd4c5e92e
Abdullah Bilgin <abilgin02@icloud.com>
2023-04-30 07:05:08 +0300

    Refactoring HistoryView (https://github.com/users/abilgin88/projects/8/views/2?pane=issue&itemId=26962943)

--- MyFit/Views/HistoryView.swift
+++ MyFit/Views/HistoryView.swift
@@ -8,15 +8,12 @@
 import SwiftUI
 
 struct HistoryView: View {
-    let today = Date()
-    let yesterday = Date().addingTimeInterval(-86400)
-    let exercises1 = ["Squat", "Step Up", "Burpee", "Sun Salute"]
-    let exercises2 = ["Squat", "Step Up", "Burpee"]
+    let history = HistorryStore()
     
     var body: some View {
         ZStack (alignment: .topTrailing) {
             Button(action: {}) {
-              Image(systemName: "xmark.circle")
+                Image(systemName: "xmark.circle")
             }
             .font(.title)
             .padding()
@@ -26,22 +23,17 @@ struct HistoryView: View {
                     .font(.title)
                     .padding()
                 Form {
-                    Section(
-                        header:
-                            Text(today.formatted(as: "MMM d"))
-                            .font(.headline)) {
-                                ForEach(exercises1, id: \.self) { exercise in
-                                    Text(exercise)
-                                }
+                    ForEach(history.exerciseDays) { day in
+                        Section(
+                            header:
+                                Text(day.date.formatted(as: "MMM d"))
+                                .font(.headline))
+                        {
+                            ForEach(day.exercises, id: \.self) { exercise in
+                                Text(exercise)
                             }
-                    Section(
-                        header:
-                            Text(yesterday.formatted(as: "MMM d"))
-                            .font(.headline)) {
-                                ForEach(exercises2, id: \.self) { exercise in
-                                    Text(exercise)
-                                }
-                            }
+                        }
+                    }
                 }
             }
         }
